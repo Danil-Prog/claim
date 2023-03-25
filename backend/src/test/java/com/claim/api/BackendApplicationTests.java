9c3b62a5cb7ab5418a5edf450cebcb5340c53fd0
Danil <dany.gavrik@mail.ru>
2023-03-26 02:41:30 +0700

    init

---
+++ backend/src/test/java/com/claim/api/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
