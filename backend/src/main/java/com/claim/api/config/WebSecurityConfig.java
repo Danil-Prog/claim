8893ff9a6d82c1445c791692ec650f260fbdfe27
Danil-Prog <dany.gavrik@mail.ru>
2023-03-27 23:02:06 +0700

    разграничены права доступа для теста

--- backend/src/main/java/com/claim/api/config/WebSecurityConfig.java
+++ backend/src/main/java/com/claim/api/config/WebSecurityConfig.java
@@ -3,22 +3,50 @@ package com.claim.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic();
+
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/home")
+                .hasRole("USER")
+                .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll());
         return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("root")
+                .password(passwordEncoder().encode("root"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
