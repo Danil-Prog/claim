ae94fa8d1d4f273e9df3592c6fba968c086243a7
Danil-Prog <dany.gavrik@mail.ru>
2023-03-31 23:38:23 +0700

    добавил регистрацию пользователей

--- backend/src/main/java/com/claim/api/repository/UserRepository.java
+++ backend/src/main/java/com/claim/api/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.claim.api.repository;
 
+import com.claim.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLogin(String login);
 }
--- backend/src/main/java/com/claim/api/service/UserService.java
+++ backend/src/main/java/com/claim/api/service/UserService.java
@@ -1,14 +1,43 @@
 package com.claim.api.service;
 
+import com.claim.api.entity.Role;
+import com.claim.api.entity.User;
+import com.claim.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login);
+    }
+
+    public boolean saveUser(User user){
+        User userFromDataBase = userRepository.findByLogin(user.getUsername());
+
+        if (userFromDataBase != null){
+            return false;
+        }
+        user.setRole(user.getRole());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
     }
 }
