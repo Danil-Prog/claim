06db9f0217e6bcd1ecee90866ba5cbeb731407e5
chswell <nikitastreltsov324@gmail.com>
2023-04-11 11:52:19 +0700

    test create user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import './index.scss';
+import CreateUser from './components/CreateUser'
 
 function App() {
   return (
-    <div className="container">
-      <div className="wrapper">
-        <form id="signin" method="post">
-          <input type="text" id="user" name="user" placeholder="username" />
-          <input type="password" id="pass" name="pass" placeholder="password" />
-          <button type="submit">&#xf0da;</button>
-        </form>
-      </div>
-    </div>
+    // <div className="container">
+    //   <div className="wrapper">
+    //     <form id="signin" method="post">
+    //       <input type="text" id="user" name="user" placeholder="username" />
+    //       <input type="password" id="pass" name="pass" placeholder="password" />
+    //       <button type="submit">&#xf0da;</button>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <CreateUser />
   );
 }
 
---
+++ frontend/src/components/CreateUser.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios'
+
+function CreateUser() {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [role, setRole] = React.useState('');
+
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+        createUser({username, password, role});
+    }
+
+    const createUser = async (obj) => {
+        try {
+            await axios.post('http://localhost:8080/registration', obj);
+        } catch (error) {
+            console.log(error);
+        }
+       
+    }
+
+
+    return(
+        <div>
+            <form onSubmit={handlerSubmit}>
+                <label htmlFor="username">username:</label>
+                <input name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                <label htmlFor="password">password:</label>
+                <input name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <label htmlFor="role">role:</label>
+                <select name="role" value={role} onChange={(e) => setRole(e.target.value)}>
+                    <option value="ROLE_USER">Пользователь</option>
+                    <option value="ROLE_EXEC">Исполнитель</option>
+                    <option value="ROLE_ADMIN">Администратор</option>
+                </select>
+                <input type="submit" value="Отправить"/>
+            </form>
+        </div>
+    );
+}
+
+export default CreateUser;
