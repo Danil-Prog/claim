c00df9b744b62eabe08eff91eea7d0bff773ea68
Danil-Prog <dany.gavrik@mail.ru>
2023-04-15 22:47:05 +0700

    добавил методы добавления задачи от пользователя и получения всех задач по id пользователя

--- backend/src/main/java/com/claim/api/controller/TaskController.java
+++ backend/src/main/java/com/claim/api/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.claim.api.controller;
 
 
+import com.claim.api.entity.Task;
 import com.claim.api.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/task")
@@ -15,5 +18,15 @@ public class TaskController {
     @Autowired
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @GetMapping("/user={id}")
+    public ResponseEntity<Set<Task>> getTasksByUserId(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTasksByUserId(id));
+    }
+
+    @PostMapping("/user={id}")
+    public ResponseEntity<Task> createTask(@PathVariable Long id, @RequestBody Task task) {
+        return ResponseEntity.ok(taskService.createTask(id, task));
     }
 }
