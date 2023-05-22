ee79d099d40e36b84c86bc6a32cef62c65fe3c7d
Daniil <56443917+Danil-Prog@users.noreply.github.com>
2023-05-22 23:52:18 +0700

    Revert "update jar file"

--- backend/src/main/java/com/claim/api/BackendApplication.java
+++ backend/src/main/java/com/claim/api/BackendApplication.java
@@ -1,12 +1,9 @@
 package com.claim.api;
 
-import com.claim.api.config.WebSocketConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(WebSocketConfiguration.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
