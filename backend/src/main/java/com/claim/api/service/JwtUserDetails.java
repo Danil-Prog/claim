18e53aa575eecd9f214f97170735211ab68114a8
Danil-Prog <dany.gavrik@mail.ru>
2023-06-13 21:42:52 +0700

    добавлена реализация jwt аутентификации

---
+++ backend/src/main/java/com/claim/api/service/JwtUserDetails.java
@@ -0,0 +1,20 @@
+package com.claim.api.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+
+    private final Long id;
+
+    public JwtUserDetails(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
