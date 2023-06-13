47a71f6d5b7593c7b7bb789592c8259ed8e97fe6
Danil-Prog <dany.gavrik@mail.ru>
2023-06-13 21:44:28 +0700

    refactoring

--- backend/src/main/java/com/claim/api/controller/dto/AuthenticationRequest.java
+++ backend/src/main/java/com/claim/api/controller/dto/AuthenticationRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class LoginRequest {
+public class AuthenticationRequest {
 
     @NotBlank
     private String username;
