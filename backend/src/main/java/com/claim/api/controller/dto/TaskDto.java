2672456eb117cd0a1a27b1689c3b117f43c241a8
Danil-Prog <dany.gavrik@mail.ru>
2023-07-18 00:22:39 +0700

    Обновил дто, добавив тип задачи

--- backend/src/main/java/com/claim/api/controller/dto/TaskDto.java
+++ backend/src/main/java/com/claim/api/controller/dto/TaskDto.java
@@ -1,14 +1,16 @@
 package com.claim.api.controller.dto;
 
 import com.claim.api.entity.Department;
-import com.claim.api.entity.StatusTask;
+import com.claim.api.entity.TaskStatus;
+import com.claim.api.entity.TaskType;
 
 import java.util.Date;
 
 public record TaskDto(Long id,
                        String title,
                        String description,
-                       StatusTask statusTask,
+                       TaskStatus taskStatus,
+                       TaskType taskType,
                        Department department,
                        UserDto executor,
                        Date startDate,
