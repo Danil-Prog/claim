06ffbd29f2ef56dc570eca65b17a48ec97da28e4
Danil-Prog <dany.gavrik@mail.ru>
2023-08-01 23:18:18 +0700

    Добавлена валидация для пространства

--- backend/src/main/java/com/claim/api/entity/Department.java
+++ backend/src/main/java/com/claim/api/entity/Department.java
@@ -3,6 +3,7 @@ package com.claim.api.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,8 +25,10 @@ public class Department {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "department_name")
+    @Size(min = 3, max = 25)
     private String name;
     @Column(name = "short_name")
+    @Size(min = 2, max = 7)
     private String shortName;
     @Column(name = "image")
     private String image;
