95de62a10a6d9c20304991723b47f7d6cb563d6c
Danil-Prog <dany.gavrik@mail.ru>
2023-09-09 17:17:14 +0700

    Добавлено логгирование и метод изменения текста для автора комментария

--- backend/src/main/java/com/claim/api/service/CommentService.java
+++ backend/src/main/java/com/claim/api/service/CommentService.java
@@ -1,12 +1,15 @@
 package com.claim.api.service;
 
 import com.claim.api.controller.dto.CommentDto;
+import com.claim.api.controller.request.CommentUpdateRequest;
 import com.claim.api.entity.Comment;
 import com.claim.api.entity.Issue;
 import com.claim.api.entity.User;
 import com.claim.api.exception.BadRequestException;
 import com.claim.api.mapper.CommentMapper;
 import com.claim.api.repository.CommentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -16,6 +19,7 @@ import java.util.Optional;
 @Service
 public class CommentService {
 
+    private final Logger logger = LoggerFactory.getLogger(CommentService.class);
     private final IssueService issueService;
     private final CommentMapper commentMapper;
     private final UserService userService;
@@ -50,6 +54,7 @@ public class CommentService {
         issue.setComments(comment);
         issueService.updateIssue(issue);
 
+        logger.info("User '{}' created new comment: {}", principal.getName(), comment.getText());
         return commentMapper.toCommentDto(comment);
     }
 
@@ -61,7 +66,22 @@ public class CommentService {
             issue.getComments().remove(comment);
             commentRepository.deleteById(commentId);
             return commentMapper.toCommentDto(comment);
+        } else {
+            logger.warn("User {} tried to delete a comment (comment_id: {}) in which he is not the author", principal.getName(), comment.getId());
+            throw new BadRequestException("User " + user.getUsername() + "is not the author of the comment, deletion is not possible");
+        }
+    }
+
+    public CommentDto updateTextComment(Long commentId,
+                                        CommentUpdateRequest commentUpdateRequest,
+                                        Principal principal) {
+        User user = userService.getUserByUsername(principal.getName());
+        Comment comment = this.getCommentById(commentId);
+        if (comment.getSender().equals(user)) {
+            comment.setText(commentUpdateRequest.getText());
+            commentRepository.save(comment);
+            return commentMapper.toCommentDto(comment);
         } else
-            throw new BadRequestException("User" + user.getUsername() + "is not the author of the comment, deletion is not possible");
+            throw new BadRequestException("User " + user.getUsername() + "is not the author of the comment, deletion is not possible");
     }
 }
