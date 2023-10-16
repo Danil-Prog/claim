a21f71cfc8b9b096305404f05ef437633315be59
Гаврилов Даниил Геннадьевич <danygavrik05@gmail.com>
2023-10-16 18:34:20 +0700

    new field is_visible

--- backend/src/main/java/com/claim/api/entity/attachment/Attachment.java
+++ backend/src/main/java/com/claim/api/entity/attachment/Attachment.java
@@ -14,33 +14,45 @@ import java.util.Date;
 @EqualsAndHashCode
 @Table(name = "attachament")
 public class Attachment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
     @Column(name = "name")
     private String name;
+
     @NotNull
     @Column(name = "original_name")
     private String originalName;
+
     @NotNull
     @Column(name = "type")
     @Enumerated(EnumType.STRING)
     private AttachmentType attachmentType;
+
     @Column(name = "content_type")
     private String contentType;
+
     @Column(name = "url")
     private String url;
+
     @NotNull
     @Column(name = "size")
     private double size;
+
     @NotNull
     @Column(name = "date_loaded")
     private Date dateLoaded = new Date();
+
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
     private Profile uploaded;
+
+    @Column(name = "is_visible")
+    private boolean isVisible = true;
 
     public Attachment() {
     }
